46d43deaa3d8bfe93884f3874a0b24e0157e357b
dgarcia <danielgv2404@gmail.com>
2026-04-14 02:26:11 -0600

    frontend

--- API/Program.cs
+++ API/Program.cs
@@ -11,12 +11,16 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
+app.UseCors( opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
 
 DbInitializer.InitDb(app);
 
